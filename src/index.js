52d3b8e81d4438b2b0e8446bd0d73908c0799d96
Alexey Seryogin <xellex.yung@yandex.ru>
2020-10-03 21:42:19 +0300

    fix: add functions min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        let minimum
+        array.forEach(element => {
+            if (typeof(element) === 'number' && !isNaN(element))
+                if (typeof(minimum) === 'number') {
+                    if (element < minimum) minimum = element
+                } else minimum = element
+        })
+        if (typeof(minimum) === 'number' && !isNaN(minimum)) return minimum
+    }
+    return 0
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        let maximum
+        array.forEach(element => {
+            if (typeof(element) === 'number' && !isNaN(element))
+                if (typeof(maximum) === 'number') {
+                    if (element > maximum) maximum = element
+                } else maximum = element
+        })
+        if (typeof(maximum) === 'number' && !isNaN(maximum)) return maximum
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        const sum = sum(array)
+        return sum / array.length
+    }
+    return 0
 }
